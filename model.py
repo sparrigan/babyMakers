cd6b95f3d02c0a31b723bb7a53429bc72e54b709
Nicholas Harrigan <sparrigan@gmail.com>
2015-12-30 19:55:49 +0000

    Two databases model (m/f). Has search method and is tidied up a bit

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 # Imports needed
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, ForeignKey
+from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, DateTime, Text
 from sqlalchemy.orm import sessionmaker
 import pandas as pd
@@ -16,7 +16,7 @@ import time
 #Drop all tables one at a time:
 # psql -U 'test_user' 'test' -t -c "select 'drop table \"' || tablename || '\" cascade;' from pg_tables where schemaname = 'public'"  | psql -U 'test_user' 'test'
 
-
+#Connect to DB with SQLA
 engine = create_engine("postgresql://test_user:testpw@localhost/test", echo=False)
 Base = declarative_base(engine)
 # TODO: does each individual table need it's own metadata
@@ -25,6 +25,10 @@ Session = sessionmaker(bind=engine)
 session = Session()
 year_vals = np.arange(1880,2011)
 
+# def chunks(l, n):
+#     """Yield successive n-sized chunks from l."""
+#     for i in xrange(0, len(l), n):
+#         yield l[i:i+n]
 
 def _csv_df(path):
 	"""Import CSV to dataframe in correct format, including filling NAs
@@ -58,7 +62,6 @@ def _get_csv_name_vals(name, names_groupby):
 	return male_vals.reindex(years, fill_value=0), \
 		fmale_vals.reindex(years, fill_value=0)
 
-
 def _drop_tabs(s):
 	"""Drop all tables from DB using SQLA expression language
 	s: SQLA session to emmit SQL with"""
@@ -67,20 +70,6 @@ def _drop_tabs(s):
 	s.execute("CREATE SCHEMA public;")
 	s.commit()
 	#TODO: Add alternate that deletes each table individually if too many locks
-
-#Create classes for male and female tables
-def _create_tabs_new():
-	col_dic_m = {'__tablename__': 'male', 'name': Column('name', String(32), primary_key=True)}
-	col_dic_f = {'__tablename__': 'female', 'name': Column('name', String(32), primary_key=True)}
-	for year in year_vals:
-		col_dic_m['col'+str(year)] = Column(str(year), Integer)
-		col_dic_f['col'+str(year)] = Column(str(year), Integer)
-	Male = type('Male', (Base,), col_dic_m)
-	Female = type('Female', (Base,), col_dic_f)
-	#Create corresponding tables in database
-	#TODO: Double check that this doesn't try and create new tables if already
-	#finds __tablename__ in the DB
-	Base.metadata.create_all(engine)
 
 def _insert_all_rows(names_list, names_df, chunk_size):
 	"""Inserts rows into male and female tables for each name"""
@@ -112,211 +101,49 @@ def _insert_all_rows(names_list, names_df, chunk_size):
 		# 	session.commit()
 		# 	i += 1
 
+def get_name_data(name, sex):
+	if sex in ['m', 'M', 'male', 'Male']:
+		qry = session.query(Male).filter_by(name=name)
+		if qry.first():
+			return qry.first().__dict__
+		else:
+			return None
+	elif sex in ['f', 'F', 'female', 'Female']:
+		qry = session.query(Female).filter_by(name=name)
+		if qry.first():
+			return qry.first().__dict__
+		else:
+			return None
+	#Code to get ordered list (by year) instead of dict
+	#q_dict = qry.first().__dict__
+	#[q_dict['col'+str(yr)] for yr in range(1880,2011)]
 
-#ONE FUNCTION THAT CREATES ALL CLASSES AND COMMITS TO TABLES AND IS CALLED IN NAME==MAIN
-#(PUT CREATE TABLES IN SEPERATE FUNCTION)
-
-#ANOTHER FUNCTION THAT CREATES REQUIRED CLASS OF REQUIRED NAME. THIS IS CALLED EXTERNALLY
-#(OR GLOBALLY IF WANTED TO ENSURE CLASS AVAILABLE AS PACKAGE IMPORTED)
-
-def create_tab_class(name):
-	"""Creates class mapped to table in db with given name"""
-	# Use declarative and autoload to construct a class for the table we want
-	# Alternatively could make Table obj and link to class with mapper or
-	# define all table columns etc explicitly if known
-	# http://tinyurl.com/2allx6z
-	return type(str(name), (Base,), {
-		'__tablename__': name,
-		'__table_args__': {'autoload':True}})
-
-
-def get_name_data(name, sex=None):
-	"""Returns series/df of births for given name. If sex specified then returns
-	series of this only. Otherwise returns df with both sexes"""
-	#This function will create ORM class for this name, but pandas can get
-	#table just given it's name and engine connection! So don't need it.
-	# name_class = create_tab_class(name)
-	pk_name = name+'_ID'
-	#Use pandas to return df from named table
-	name_df = pd.read_sql_table(name, engine)
-	#Set primary key years as index
-	name_df = name_df.set_index(pk_name)
-	name_df.index.name = 'Year'
-	#If only one sex requested then select it out
-	if (sex=='M' or sex=='m'):
-		return name_df.m
-	elif (sex=='F' or sex=='f'):
-		return name_df.f
-	else:
-		return name_df
-
-
-def _create_tables_core(names_list, names_df):
-	"""Use core settings"""
-	year_values = np.arange(1880,2011)
-	names_gb = names_df.groupby(names_df.name)
-	chunk_size = 1000
-	time_df = pd.DataFrame(columns=['time_class', 'time_insert'],
-		index=range(chunk_size,chunk_size*300,chunk_size))
-	chunker = chunks(names_list, chunk_size)
-	# connection = engine.connect()
-	ii=0
-	for chunk in chunker:
-		print 'Chunk: %r to %r' %(ii*chunk_size, (ii+1)*chunk_size)
-		start = time.time()
-		# 1. Create table objects for current chunk
-		table_list = []
-		for name in chunk:
-			pkname = name+'_ID'
-			table_list.append(Table(name, Base.metadata,
-				Column(pkname, Integer, primary_key=True),
-				Column('m', Integer),
-				Column('f', Integer)
-			))
-
-		Base.metadata.create_all(engine)
-		print 'Time to create tables: %r' %int((time.time() - start))
-		time_df.ix[(ii+1)*chunk_size].time_class = int((time.time() - start) * 1000)
-
-		# Insert values into tables
-		start = time.time()
-		for tab in table_list:
-			tabname = tab.name
-			m_values, f_values = _get_csv_name_vals(tabname, names_gb)
-			pkname = tabname+'_ID'
-			#Construct string for insert containing all entries for this table
-			ins_vals = zip(year_values, m_values.births.values, f_values.births.values)
-			ins_vals_str = str(ins_vals)[1:-1]
-			ins_statement = 'INSERT INTO "%s" VALUES %s' %(tabname, ins_vals_str)
-			session.execute(ins_statement)
-			#Note: *DO* need to commit after an execute statement
-			session.commit()
-			# for i,j,k in zip(year_values, m_values.births.values, f_values.births.values):
-			# 	#Note: pass keywords as an unpacked dict for dyanmic pk keyword
-			# 	# post = name_class(**{pk_name: i, 'm':j, 'f': k})
-			# 	session.execute('INSERT INTO "%s" VALUES (%r, %r, %r)' %(tabname, i, j, k))
-		# session.commit()
-			# connection.execute(
-			# 	tab.insert(),
-			# 	#TODO: Rewrite this to insert rows with year, m, f values
-			# 	[{pkname: i, 'm': j, 'f': k} for i,j,k in zip(year_values, m_values.births.values, f_values.births.values)]
-			# )
-		print 'Time to insert values: %r' %int((time.time() - start))
-		time_df.ix[(ii+1)*chunk_size].time_insert = int((time.time() - start) * 1000)
-
-		time_df.to_csv('/Users/nicholasharrigan/code/babyMakers/times.csv')
-		ii+=1
-	# connection.close()
-
-def chunks(l, n):
-    """Yield successive n-sized chunks from l."""
-    for i in xrange(0, len(l), n):
-        yield l[i:i+n]
-
-# for chunk in chunker:
-# 	#print 'chunk: %r' %chunk
-# 	class_list = []
-# 	for num in chunk:
-# 		class_list.append('num '+str(num))
-# 	print class_list
-# 	print ''
-
-def _create_tables(names_list, names_df):
-	"""Create tables for every name in DF"""
-	#Create groupby object of names
-	names_gb = names_df.groupby(names_df.name)
-	#Loop over batches of names and create class,write,table for each
-	i = 0
-	total = len(names_list)
-	chunker = chunks(names_list, 1000)
-	for chunk in chunker:
-		start = time.time()
-		class_list = []
-		# 1. Create declarative based classes
-		for name in chunk:
-			# Name of primary key column for this tables year columns
-			pk_name = name+'_ID'
-			#Create classes for our tables. Note that by subclassing Base
-			#sqlalchemy mapper automatically creates assoc. Table objects for us.
-			class_list.append(type(str(name), (Base,), {
-				'__tablename__': name,
-				pk_name: Column(Integer, primary_key=True),
-				'm': Column(Integer, nullable=True),
-				'f': Column(Integer, nullable=True)}))
-		# 2. Create tables for this batch of classes in DB
-		Base.metadata.create_all(engine)
-		# 3. Commit values to this batch of tables
-		_populate_tables(class_list, names_gb)
-		i += 1
-		print str(i)+'000 out of %r' %total
-		print 'Time this iteration: %r' %int((time.time() - start) * 1000)
-
-	# NOTE: If wanted to dynamically generate columns instead then follow this:
-	# http://stackoverflow.com/questions/2574105/sqlalchemy-dynamic-mapping/2575016#2575016
-
-	#NOTE: How do we chunk table creation so don't have lots of python classes
-	# in memory.
-	# rewrite the variable value for the classes and call on the next chunk
-	#  (ie class_dict
-
-
-
-def _populate_tables(class_list, names_gb):
-	"""Fill tables with values from DF
-	class_dict: dictionary of ORM classes for each table in DB
-	names_gb: groupby object of names_df"""
-	# Years we will consider
-	year_values = np.arange(1880,2011)
-	# Loop over tables objects and assign values from csv
-	for name_class in class_list:
-		name = name_class.__tablename__
-		#Get male and female values for this name
-		m_values, f_values = _get_csv_name_vals(name, names_gb)
-		pk_name = name+'_ID'
-		#Generate list of row instances of our mapper classes to add
-		add_list = []
-		for i,j,k in zip(year_values, m_values.births.values, f_values.births.values):
-			#Note: pass keywords as an unpacked dict for dyanmic pk keyword
-			#add_list.append(name_class(**{pk_name: i, 'm':j, 'f': k}))
-			post = name_class(**{pk_name: i, 'm':j, 'f': k})
-			session.add(post)
-
-		# #Add all rows using add_all command
-		# session.add_all(add_list)
-		#Force commit of all changes
-	session.commit()
+#Create classes for male and female tables
+col_dic_m = {'__tablename__': 'male', 'name': Column('name', String(32), primary_key=True)}
+col_dic_f = {'__tablename__': 'female', 'name': Column('name', String(32), primary_key=True)}
+for year in year_vals:
+	col_dic_m['col'+str(year)] = Column(str(year), Integer)
+	col_dic_f['col'+str(year)] = Column(str(year), Integer)
+Male = type('Male', (Base,), col_dic_m)
+Female = type('Female', (Base,), col_dic_f)
+#Create corresponding tables in database
+#TODO: Double check that this doesn't try and create new tables if already
+#finds __tablename__ in the DB
+Base.metadata.create_all(engine)
 
 if __name__ == '__main__':
-
-	_create_tabs_new()
-	#CSV path
-	path = '/Users/nicholasharrigan/pydata-book-master/ch02/names/'
-	#Import CSV file and put into pandas df
-	print 'Importing CSV...'
-	names_df = _csv_df(path)
 	#WARNING! Clear db
-	print 'Dropping tables...'
+	# print 'Dropping tables...'
 	# _drop_tabs(session)
 
+	#Import CSV data in pandas DF
+	#CSV path
+	path = '/Users/nicholasharrigan/pydata-book-master/ch02/names/'
+	print 'Importing CSV...'
+	names_df = _csv_df(path)
+
+	#Get list of names (each form a record) for DB from pandas DF
 	names_list = names_df.name.unique()
 	#Insert rows
 	print 'Inserting rows'
 	_insert_all_rows(names_list, names_df, 0)
-
-
-
-
-
-
-	# #Create classes and mappings for new tables based on CSV entries
-	# #TODO: Modify create_tables to work with pandas df instead of simple csv
-	# #Get list of unique names and create tables in db with these names
-	# print 'Generating names list'
-	# names_list = names_df.name.unique()
-	# # names_list = ['Alan','Betty']
-	# print 'Creating tables...'
-	# _create_tables_core(names_list, names_df)
-	# # _create_tables(names_list, names_df)
-	# #Populate new tables and commit to db
-	# # print 'Populating tables...'
-	# # _populate_tables(tabs, names_df)
