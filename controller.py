7f806885bf7f38e55c3b62143161cde267ca8d19
Nicholas Harrigan <sparrigan@gmail.com>
2016-01-31 14:04:37 +0000

    Tidied code

--- controller.py
+++ controller.py
@@ -1,34 +1,34 @@
 import model
+import os
 from flask import Flask
 from flask import render_template, request, redirect, url_for, flash, jsonify
+import json
 import urllib2
 from urllib import quote
-from math import sqrt
-import pandas as pd
-from scipy import ndimage as nd
-import json
 import time
+import pandas as pd
 import numpy as np
-from numpy.random import random_sample
-import os
-
 from numpy import random
+from numpy.random import random_sample
+from scipy import ndimage as nd
 import math
+from math import sqrt
+
 # TODO: add wtfworms import wtforms
+
 #Create flask instance
 application = Flask(__name__)
 
 # Import environment configuration settings for API
-# app.config.from_envvar('BABY_MAKERS_SETTINGS', silent=True)
-# API_KEY = app.config['API_KEY']
 API_KEY = os.environ['API_KEY']
+
+#T= themoviedb base URL
+base_url = "http://api.themoviedb.org/3/"
 
 
 def get_celeb_score(data, celeb_years, alpha=0.95):
 	"""Takes a dict of year:births *for years wanted*, and a list of movie years within
 	this range"""
-	# NOTE: Check that we have zeros where we should in babyname data
-	# NOTE: Are assuming dict of years:births over range we want to consider
 	# Convert birth data dict to a pandas Series
 	birth_data = pd.Series(data)
 	birth_data.sort_index()
@@ -110,16 +110,10 @@ def get_percentile_points(hist_vals, hist_bins, percentile_value, celeb_score=No
 	return celeb_pval, hist_df.ix[percentile_vals.index]
 
 def check_str(name):
-	#Make sure camel case
+	#Make sure camel case for DB
 	new_name = name.lower().capitalize()
 	return new_name
 
-# def get_movie_ids(json_vals):
-# 	"""Takes a json page and returns dict with movie names and ids"""
-# 	ret_dict = {}
-# 	for movie in json_vals['results']:
-# 		ret_dict[movie['title']] = [movie['id'], int(movie['release_date'][:4])]
-# 	return ret_dict
 
 def get_movie_ids(json_vals):
 	"""Takes a json page and returns list of dicts with movie names and ids"""
@@ -127,7 +121,7 @@ def get_movie_ids(json_vals):
 	for movie in json_vals['results']:
 		# Only add movie to list if json contains info needed in right format
 		if ((len(movie['title'])>0) & (isInt_str(movie['id'])) & (isInt_str(movie['release_date'][:4]))):
-			# REMOVE THIS WHEN UPDATE DATABASE!?!?!?
+			#NOTE: REMOVE THIS WHEN UPDATE DATABASE!?!?!?
 			if (int(movie['release_date'][:4]) <= 2010):
 				mov_inf = {'info': {'title': movie['title'], 'm_id': movie['id'], 'release': int(movie['release_date'][:4]), 'poster': None}}
 				# Add on poster_path if it exists otherwise leave as None
@@ -139,22 +133,21 @@ def get_movie_ids(json_vals):
 
 
 def isInt_str(v):
-	''' Checks if string contains an int'''
+	"""Checks if a string contains an int"""
 	v = str(v).strip()
 	return v=='0' or (v if v.find('..') > -1 else v.lstrip('-+').rstrip('0').rstrip('.')).isdigit()
 
-base_url = "http://api.themoviedb.org/3/"
 
 def remove_repeats(movie_dict):
+	"""Removes any repeated entries in json received from themoviedb API"""
 	# Get list of ids in same order as list of movie dics
 	id_list = [x['info']['m_id'] for x in movie_dict]
-
 	# Create dict of occurences of each number
 	y = np.bincount(id_list)
 	ii = np.nonzero(y)[0]
 	count_dic = dict(zip(ii,y[ii]))
 	del_idx = []
-	# Now loop through movie_dict entries, remove if more than one occurences
+	# Now loop through movie_dict entries, remove if >1 occurences
 	for idx, mov in enumerate(movie_dict):
 		if count_dic[mov['info']['m_id']] > 1:
 			# log entry for delete
@@ -164,30 +157,26 @@ def remove_repeats(movie_dict):
 	# Now delete those entries from list in reverse
 	for index in sorted(del_idx, reverse=True):
 		del movie_dict[index]
-
 	return movie_dict
 
 # TODO: Upadate to return suggestions if not unique result for name
 def get_movieapi_results(full_name):
 	"""Takes full name string (not URL encoded) and returns dict of movies"""
 	# Convert name to URL encoded string
-	# print full_name
 	full_name = quote(full_name)
-	# print full_name
+	# Get actor_id for movie star searched for
 	name_url = "http://api.themoviedb.org/3/search/person?&api_key="+API_KEY+"&query=%s" %full_name
-	print name_url
 	response = urllib2.urlopen(name_url)
 	data = json.load(response)
 	actor_id = data['results'][0]['id']
 	profile_url = None
-	# Assign profile pic url if it exists
+	# Assign movie-star profile pic url if it exists
 	if ('profile_path' in data['results'][0]):
 		if data['results'][0]['profile_path']:
 			profile_url = data['results'][0]['profile_path']
-	# print actor_id
+	# Get list of all movies for movie-star using actor_id
 	query = 'movie?with_cast=%i' %(actor_id)
 	url = base_url + 'discover/' + query + "&api_key=" + API_KEY
-	# print url
 	# Response for query for actors films will contain multiple pages.
 	# Get first page to find num of pages
 	response2 = urllib2.urlopen(url)
@@ -196,7 +185,6 @@ def get_movieapi_results(full_name):
 	movie_dict = get_movie_ids(pg1)
 	#Now iterate over all remaining pages, adding movies to movie_dict dictionary
 	temp = []
-
 	for page in range(2,num_pages+1):
 		#Get current page
 		pagereq = '&page=%i' %page
@@ -205,22 +193,15 @@ def get_movieapi_results(full_name):
 		temp.append(current_pg)
 		#Combine dict of movies from this page with previously found ones
 		movie_dict += get_movie_ids(current_pg)
-
-	# Remove repeated entries by movie_id and return
-	# Note: turn this on if dealing with themoviedb bug on ordering searches
-
+	# Remove entries with repeated movie_id (themoviedb confirmed bug)
 	movie_dict = remove_repeats(movie_dict)
-
+	# Return movie_dict, actor_id and profile picture url
 	return movie_dict, actor_id, profile_url
-
-# TODO: Way to refactor that reduces the number of movies we query for
-# more detailed information?
-# TODO: Replace this with a neural network!
 
 def get_cast_pos(actor_id, movie_id, cast_id_min):
 	"""Returns T/F for whether actor in top n of cast list"""
+	# Get full credit list for movie in question from themoviedb
 	query = base_url + 'movie/' + str(movie_id) + '/credits?' + "&api_key=" + API_KEY
-
 	try:
 		page = urllib2.urlopen(query)
 	except urllib2.HTTPError, err:
@@ -229,44 +210,43 @@ def get_cast_pos(actor_id, movie_id, cast_id_min):
 	except urllib2.URLError, err:
 		api_error = err.reason
 		page = None
-
+	# Allow for possibility that no entry
 	if page:
 		try:
 			movie_cast = json.load(page)
 		except ValueError:
 			error = 'ValueError loading json'
 			movie_cast = None
-
-
+		# If received list of cast members, search for desired movie-star
 		if movie_cast:
+			# Extract cast data needed for check from json into list
 			if movie_cast['cast']:
 				castid = [act for act in movie_cast['cast'] if act['id']==actor_id][0]['order']
-				# If no cast data then assume this movie isn't worth considering and force a False return
 			else:
-				# If no cast information then assume this movie isn't worth considering
+				# If no cast data then assume movie not worth considering
 				return False, None
+			# If movie-star high enough in cast ret True, else ret False
 			if (castid < cast_id_min):
 				return True, None
 			else:
 				return False, None
 		else:
-			# Error decoding JSON, return None
+			# Error decoding JSON
 			return None, error
 	else:
+		# Error in connection
 		return None, api_error
 
-# This has rest of logic for deciding on film
+
 # TODO: Include error catching for these api calls too (put error catching into function)
 
 def get_movie_score(movie_id):
 	"""Checks whether movie passed satsifies criteria for being significant
 	in actors career
-	If actor in top n credits, returns revenue, average vote and popularity"""
+	Returns revenue, average vote and popularity"""
 	# Get movie info from api call
-	# print movie_id
 	query = base_url + 'movie/' + str(movie_id) +'?' + "&api_key=" + API_KEY
 	score_json = json.load(urllib2.urlopen(query))
-
 	# Get poularity ranking from themoviedb:
 	if 'popularity' in score_json:
 		pop = score_json['popularity']
@@ -282,7 +262,7 @@ def get_movie_score(movie_id):
 		rev = score_json['revenue']
 	else:
 		rev = 0
-	# Return values
+	# Return vals
 	return {'revenue': rev, 'pop': pop, 'vote': vote}
 
 
@@ -301,69 +281,48 @@ def top_n_movies(full_name, actor_id, n, score_func):
 			# If movie valid, pass data to score_func and store score
 			valid_movies[name] = score_func(*check)
 
+# Flask routing:
 
-
-
-
-
-# TODO: Create route for index page
 @application.route('/', methods=['GET'])
 def index():
+	"""Render index template"""
 	return render_template('index.html')
 
-@application.route('/test', methods=['GET'])
-def test():
-	return render_template('test.html')
+# @application.route('/test', methods=['GET'])
+# def test():
+# 	return render_template('test.html')
 
-@application.route('/return_list', methods=['GET'])
-def return_list():
-	return render_template('list.html')
+# @application.route('/return_list', methods=['GET'])
+# def return_list():
+# 	return render_template('list.html')
 
-@application.route('/get_d3_data/<name>/<sex>/<start_yr>', methods=['GET', 'POST'])
-def get_d3_data(name, sex, start_yr):
+@application.route('/get_name_data/<name>/<sex>/<start_yr>', methods=['GET', 'POST'])
+def get_name_data(name, sex, start_yr):
+	"""Return normed DB info for name history"""
 	# Get raw name data from database
 	data_list = model.get_name_data(name, sex, start_yr, 'python_dict')
 	# Get total birth data to normalise
 	totals = model.get_total_births(sex)
-	print "DATA BEFORE NORM = ", data_list
-	# dict comprehension
+	# Pass normed data into dict comprehension
 	normed_dict = {yr:(data_list[yr]/float(totals[int(yr)]))*100 for yr in data_list.keys()}
-	print "DATA AFTER NORM = ", normed_dict
 	# TODO: Note that this does not return a second error parameter,
 	# which d3.json function expects normally (eg: see use of d3.json here:
 	# http://www.brettdangerfield.com/post/realtime_data_tag_cloud/)
 	return jsonify(**normed_dict)
-	# return jsonify(**data_list)
 
 @application.route('/get_movie_data/<f_name>/<l_name>', methods=['GET', 'POST'])
 def get_movie_data(f_name, l_name):
+	"""Return movies and profile pic for movie-star using themoviedb"""
 	movie_dict, actor_id, profile_url = get_movieapi_results(f_name+" "+l_name)
 	json = {'results': movie_dict, 'actor_id': actor_id, 'profile_url': profile_url}
-	# print json['results']
 	return jsonify(json)
 
-# @application.route('/promise_test/<idstr>', methods=['GET', 'POST'])
-# def promise_test(idstr):
-# 	print "Promise route fired"
-# 	# Wait a random time
-# 	slp_time = int(math.floor(random.rand()*5))+1
-# 	time.sleep(slp_time)
-# 	modstr = 'oh my, '+idstr+', how nice'
-# 	ret = bool(int(round(random.rand())))
-# 	json = {'result': ret}
-# 	return jsonify(json)
-
-# TODO: Getting a 500 (internal server) error with 4110/963!!!!
-# At very least, be prepared to deal with 500 in a way that can propogate
-# TODO: Getting empty response with http://localhost:5000/cast_check/4110/165857
 @application.route('/cast_check/<actor_id>/<movie_id>', methods=['GET', 'POST'])
 def cast_check(actor_id, movie_id):
-	# make cast_pos calls async with grequest if can't get promises
-	# to work async with nginx etc...
-	# TODO: Deal with 429 errors by addding to list and retrying at end
-	# print movie_id
+	"""Return cast position of movie-star in given movie"""
+	#NOTE: Does not currently deal with 429 errrors
 	cast_pos, error = get_cast_pos(int(actor_id), int(movie_id), 2)
-	# Sleep to prevent 429 (TODO: Need a better solution for promises)
+	# Sleep to prevent 429 themoviedb API limit errors
 	time.sleep(0.2)
 	json = {'result': cast_pos, 'error':error}
 	return jsonify(json)
@@ -371,33 +330,35 @@ def cast_check(actor_id, movie_id):
 
 @application.route('/movie_score/<movie_id>', methods=['GET', 'POST'])
 def movie_score(movie_id):
-	outcome = get_movie_score(movie_id)
-	outcome['movie_id'] = int(movie_id)
+	"""Scores a movie given its movie_id"""
+	score = get_movie_score(movie_id)
+	# Add movie_id to score for returning
+	score['movie_id'] = int(movie_id)
+	# Sleep to prevent 429 themoviedb API limit errors
 	time.sleep(0.2)
-	return jsonify(outcome)
+	return jsonify(score)
 
-# TODO: Create route for getting data on user input
-# Only need route for talking to my own backend
-@application.route('/get_data', methods=["GET", "POST"])
-def get_data():
-	name = request.form['name']
-	#Validate input
-	name = check_str(name)
-	data_list = model.get_name_data(name, 'F', 'python')
-	return render_template('data_viz.html', data_list=data_list)
-
+# @application.route('/get_data', methods=["GET", "POST"])
+# def get_data():
+# 	name = request.form['name']
+# 	#Validate input
+# 	name = check_str(name)
+# 	data_list = model.get_name_data(name, 'F', 'python')
+# 	return render_template('data_viz.html', data_list=data_list)
 
 @application.route('/get_celeb_score', methods=["GET", "POST"])
 def celeb_score_route():
-	# Use get_json to return dict from json
+	"""Takes json that contains name history and also movie-star top five yrs"""
+	# Use get_json to return dict from json received by POST
 	celeb_json = request.get_json(force=True)
-	# Put births into dict
+	# Put births from POST json into dict
 	births_dict = {year:births for year,births in celeb_json['baby_vals']}
+	# Get score, p-value and histogram data for celebrity
 	celeb_pval, score, hv, hb, perc = get_celeb_score(births_dict, celeb_json['celeb_yrs'])
-	#Extract posted data from json
 	return jsonify({'celeb_pval': celeb_pval, 'celeb_score': score, 'hist_vals':hv, 'hist_bins':hb, 'perc':perc})
 
 if __name__ == '__main__':
 	application.debug = True
 	application.run()
+	# Use below for serving to all IPs when deploying
 	# application.run(host = '0.0.0.0')
--- model.py
+++ model.py
@@ -10,27 +10,41 @@ import time
 import json
 import os
 
+# Get environment variables needed to access DB
 PG_USERNAME = os.environ.get('PG_USERNAME')
 PG_PASSWORD = os.environ.get('PG_PASSWORD')
 DATABASE = os.environ.get('DATABASE')
 
-
-#Drop all tables one at a time:
-
 #Connect to DB with SQLA
 engine = create_engine("postgresql://"+PG_USERNAME+":"+PG_PASSWORD+"@"+DATABASE, echo=False)
+# Setup declarative base
 Base = declarative_base(engine)
+# Session for connection
 Session = sessionmaker(bind=engine)
 session = Session()
 
 # TODO: Change this to dynamically find years so can update with latests csv's of data from web
-
 year_vals = np.arange(1880,2011)
 
-# def chunks(l, n):
-#     """Yield successive n-sized chunks from l."""
-#     for i in xrange(0, len(l), n):
-#         yield l[i:i+n]
+#Create classes for male and female tables
+col_dic_m = {'__tablename__': 'male', 'name': Column('name', String(32), primary_key=True)}
+col_dic_f = {'__tablename__': 'female', 'name': Column('name', String(32), primary_key=True)}
+for year in year_vals:
+	col_dic_m['col'+str(year)] = Column(str(year), Integer)
+	col_dic_f['col'+str(year)] = Column(str(year), Integer)
+Male = type('Male', (Base,), col_dic_m)
+Female = type('Female', (Base,), col_dic_f)
+
+# Create table for storing total births each year for M/F
+class TotalBirthsMale(Base):
+	__tablename__ = 'total_births_male'
+	year = Column(Integer, primary_key=True)
+	births = Column(Integer)
+
+class TotalBirthsFemale(Base):
+	__tablename__ = 'total_births_female'
+	year = Column(Integer, primary_key=True)
+	births = Column(Integer)
 
 def _update_total_births():
 	"""Reads DB to find total births each year and updates Total tables"""
@@ -40,26 +54,23 @@ def _update_total_births():
 		# Iterate over columns of table (note: iterator returns object, key
 		# property gives name of column)
 		for col in mapper.attrs:
-			# Don't include name column in sum!
+			# Don't include name column in sum
 			if (col.key != 'name'):
-				# Dynamically call the method on table class corresponding to col
+				# Dynamically call method on table class corresponding to col
 				qry = session.query(func.sum(getattr(tab[0], col.key))).first()
 				# Insert into TotalBirthsMale table
 				yr = int(col.key[3:])
 				session.add(tab[1](year=yr, births=int(qry[0])))
-				# la.append(int(qry[0]))
+		# Commit changes to table
 		session.commit()
-
-
 
 def _csv_df(path):
 	"""Import CSV to dataframe in correct format, including filling NAs
 	path: where to find csv
 	returns df and groupby object"""
-	#Get csv files for each year and compile into single df
+	#Get csv files for each year and compile into single DF
 	sep_names = []
-	years = np.arange(1880,2011)
-	for year in years:
+	for year in year_vals:
 	    fname = path + ('yob%s.txt' %year)
 	    temp = pd.read_csv(fname,names=['name','sex','births'])
 	    temp['year'] = year
@@ -73,31 +84,31 @@ def _get_csv_name_vals(name, names_groupby):
 	name: string - name to return data for
 	sex: string - sex to return data for (M/F)
 	names_groupby: pandas groupby object sorting df by name"""
-	#TODO: Get unique year values from df and create range from min and max
-	years = np.arange(1880,2011)
+	# Extract male and female values from initial DF
 	both_sex = names_groupby.get_group(name)
 	male_sex = both_sex[both_sex.sex == 'M']
 	fmale_sex = both_sex[both_sex.sex == 'F']
 	male_vals = male_sex[['births','year']].set_index('year')
 	fmale_vals = fmale_sex[['births','year']].set_index('year')
 	#Return reindexed df (to check have all year values). Fill NA with zero
-	return male_vals.reindex(years, fill_value=0), \
-		fmale_vals.reindex(years, fill_value=0)
+	return male_vals.reindex(year_vals, fill_value=0), \
+		fmale_vals.reindex(year_vals, fill_value=0)
 
 def _drop_tabs(s):
 	"""Drop all tables from DB using SQLA expression language
 	s: SQLA session to emmit SQL with"""
+	# WARNING: Drops all tables in DB
+	# NOTE: If many tables then may exceed lock limit with this method
 	s.execute("DROP SCHEMA public CASCADE;")
 	s.commit()
 	s.execute("CREATE SCHEMA public;")
 	s.commit()
-	#TODO: Add alternate that deletes each table individually if too many locks
 
 def _insert_all_rows(names_df, chunk_size):
 	"""Inserts rows into male and female tables for each name"""
-	#Get list of names (each form a record) for DB from pandas DF
+	#Get list of names for DB from pandas DF
 	names_list = names_df.name.unique()
-	#Groupby dataframe for easy access to each names time series
+	#Groupby dataframe for easy access to each name's time series
 	names_gb = names_df.groupby(names_df.name)
 	#Iterate over names and add rows and build query
 	i=1
@@ -120,27 +131,27 @@ def _insert_all_rows(names_df, chunk_size):
 			print '%r names done in %r ms' %(i, int((time.time() - start) * 1000))
 			start = time.time()
 		i+=1
-		# if  (i == chunk_size):
-		# 	session.execute(insert_string)
-		# 	session.commit()
-		# 	i += 1
 
-# TODO: Refactor this to call functions and simplify m/f flow
 def get_name_data(name, sex, start_yr, ret_type = 'json'):
+	"""Returns history of births for name in requested format
+	name: name to query in DB
+	sex: sex of name
+	start_yr: first year to query DB from (>1880)
+	ret_type: format to return birth history in"""
 	if sex in ['m', 'M', 'male', 'Male']:
+		# Query Male DB for name
 		qry = session.query(Male).filter_by(name=name)
 		if qry.first():
-			#Get list from dict that is returned
+			# Get list from dict that is returned
 			q_dict = qry.first().__dict__
-			#Remove non-column entries that might be passed by SQLA
-			#And remove col prefix from others
+			# Remove non-column entries that might be passed by SQLA
+			# and remove col prefix from others
 			for keyval in q_dict.keys():
 				if ((keyval[:3] != 'col') or (keyval[3:]<start_yr)):
 					q_dict.pop(keyval)
 				else:
 					q_dict[keyval[3:]] = q_dict.pop(keyval)
-
-			#Return required type
+			# Return required type
 			if ret_type=='python':
 				return [q_dict[yr] for yr in range(start_yr,2011)]
 			elif ret_type=='json':
@@ -149,17 +160,16 @@ def get_name_data(name, sex, start_yr, ret_type = 'json'):
 				return q_dict
 			else:
 				return None
-
 		else:
 			return None
 	elif sex in ['f', 'F', 'female', 'Female']:
-		print 'Got here in the model'
+		# Query Female DB for name
 		qry = session.query(Female).filter_by(name=name)
 		if qry.first():
-			#Get list from dict that is returned
+			# Get list from dict that is returned
 			q_dict = qry.first().__dict__
-			#Remove non-column entries that might be passed by SQLA
-			#And remove col prefix from others
+			# Remove non-column entries that might be passed by SQLA
+			# and remove col prefix from others
 			for keyval in q_dict.keys():
 				if ((keyval[:3] != 'col') or (keyval[3:]<start_yr)):
 					q_dict.pop(keyval)
@@ -175,12 +185,9 @@ def get_name_data(name, sex, start_yr, ret_type = 'json'):
 				return None
 		else:
 			return None
-	#Code to get ordered list (by year) instead of dict
-	#q_dict = qry.first().__dict__
-	#[q_dict['col'+str(yr)] for yr in range(1880,2011)]
-
 
 def get_total_births(sex):
+	"""Return total number of births each year for given sex"""
 	results_dict = {}
 	# Query relevant ORM class for M/F total births
 	if sex in ['m', 'M', 'male', 'Male']:
@@ -192,40 +199,15 @@ def get_total_births(sex):
 		results_dict[vals.year] = vals.births
 	return results_dict
 
-#Create classes for male and female tables
-col_dic_m = {'__tablename__': 'male', 'name': Column('name', String(32), primary_key=True)}
-col_dic_f = {'__tablename__': 'female', 'name': Column('name', String(32), primary_key=True)}
-for year in year_vals:
-	col_dic_m['col'+str(year)] = Column(str(year), Integer)
-	col_dic_f['col'+str(year)] = Column(str(year), Integer)
-Male = type('Male', (Base,), col_dic_m)
-Female = type('Female', (Base,), col_dic_f)
-# TODO: Could optimize by only creating m/f class when m/f requested
-
-# Create table for storing total births each year for M/F
-class TotalBirthsMale(Base):
-	__tablename__ = 'total_births_male'
-	year = Column(Integer, primary_key=True)
-	births = Column(Integer)
-
-class TotalBirthsFemale(Base):
-	__tablename__ = 'total_births_female'
-	year = Column(Integer, primary_key=True)
-	births = Column(Integer)
-
-
-
-
-
 if __name__ == '__main__':
 	#WARNING! Clear db
-	# print 'Dropping tables...'
 	# _drop_tabs(session)
+
+	# If run as script then populate tables with CSV values...
 
 	#Create corresponding tables in database
 	Base.metadata.create_all(engine)
 
-	#Import CSV data in pandas DF
 	#Get CSV path from environment variable
 	path = os.environ['CSV_PATH']
 	print 'Importing CSV...'
